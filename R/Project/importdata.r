286f08bcb56b668d1a0a74234e03b108e25b2ad0
tan0407 <45198471+tan0407@users.noreply.github.com>
2019-05-07 13:21:01 +0200

    Updated R scripts

--- R/Project/importdata.r
+++ R/Project/importdata.r
@@ -1,32 +1,34 @@
 #IMPORT DATA
-aspiAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/AspiAcc.csv',header = FALSE)
-aspiEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/AspiEMG.csv',header = FALSE)
-aspiGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/AspiGyro.csv',header = FALSE)
+aspiAcc=read.csv('../../DATA_CSV/AspiAcc.csv',header = FALSE)
+aspiEMG=read.csv('../../DATA_CSV/AspiEMG.csv',header = FALSE)
+aspiGyr=read.csv('../../DATA_CSV/AspiGyro.csv',header = FALSE)
 
- basketAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BasketAcc.csv',header = FALSE)
- basketEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BasketEMG.csv',header = FALSE)
- basketGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BasketGyro.csv',header = FALSE)
+ basketAcc=read.csv('../../DATA_CSV/BasketAcc.csv',header = FALSE)
+ basketEMG=read.csv('../../DATA_CSV/BasketEMG.csv',header = FALSE)
+ basketGyr=read.csv('../../DATA_CSV/BasketGyro.csv',header = FALSE)
 
- brushAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BrushAcc.csv',header = FALSE)
- brushEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BrushEMG.csv',header = FALSE)
- brushGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/BrushGyro.csv',header = FALSE)
+ brushAcc=read.csv('../../DATA_CSV/BrushAcc.csv',header = FALSE)
+ brushEMG=read.csv('../../DATA_CSV/BrushEMG.csv',header = FALSE)
+ brushGyr=read.csv('../../DATA_CSV/BrushGyro.csv',header = FALSE)
 
- pingpongAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/PingPongAcc.csv',header = FALSE)
- pingpongEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/PingPongEMG.csv',header = FALSE)
- pingpongGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/PingPongGyro.csv',header = FALSE)
+ pingpongAcc=read.csv('../../DATA_CSV/PingPongAcc.csv',header = FALSE)
+ pingpongEMG=read.csv('../../DATA_CSV/PingPongEMG.csv',header = FALSE)
+ pingpongGyr=read.csv('../../DATA_CSV/PingPongGyro.csv',header = FALSE)
 
- runAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/RunAcc.csv',header = FALSE)
- runEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/RunEMG.csv',header = FALSE)
- runGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/RunGyro.csv',header = FALSE)
+ runAcc=read.csv('../../DATA_CSV/RunAcc.csv',header = FALSE)
+ runEMG=read.csv('../../DATA_CSV/RunEMG.csv',header = FALSE)
+ runGyr=read.csv('../../DATA_CSV/RunGyro.csv',header = FALSE)
 
- tvAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/TVAcc.csv',header = FALSE)
- tvEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/TVEMG.csv',header = FALSE)
- tvGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/TVGyro.csv',header = FALSE)
+ tvAcc=read.csv('../../DATA_CSV/TVAcc.csv',header = FALSE)
+ tvEMG=read.csv('../../DATA_CSV/TVEMG.csv',header = FALSE)
+ tvGyr=read.csv('../../DATA_CSV/TVGyro.csv',header = FALSE)
 
- walkAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkAcc.csv',header = FALSE)
- walkEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkEMG.csv',header = FALSE)
- walkGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkGyro.csv',header = FALSE)
+ walkAcc=read.csv('../../DATA_CSV/WalkAcc.csv',header = FALSE)
+ walkEMG=read.csv('../../DATA_CSV/WalkEMG.csv',header = FALSE)
+ walkGyr=read.csv('../../DATA_CSV/WalkGyro.csv',header = FALSE)
 
- epiAcc=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkEpiAcc.csv',header = FALSE)
- epiEMG=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkEpiEMG.csv',header = FALSE)
- epiGyr=read.csv('C:/Users/lebre/OneDrive/Documents/Fac/M1/Projet_M1/DATA_CSV/WalkEpiGyro.csv',header = FALSE)
+ epiAcc=read.csv('../../DATA_CSV/WalkEpiAcc.csv',header = FALSE)
+ epiEMG=read.csv('../../DATA_CSV/WalkEpiEMG.csv',header = FALSE)
+ epiGyr=read.csv('../../DATA_CSV/WalkEpiGyro.csv',header = FALSE)
+
+ 
--- R/Project/quantization.R
+++ R/Project/quantization.R
@@ -1,24 +1,55 @@
 #Quantification
 
-s1=150;
-s2=460;
-pingquantix = pingpongGyr[1];
-nrow(pingquantix);
-
-for (i in 1:nrow(pingquantix)){
-  if (pingquantix[i,1] > s2){
-    pingquantix[i,1]=1;
+# s1=150;
+# pingquantix = pingpongGyr[1];
+# nrow(pingquantix);
+# 
+# for (i in 1:nrow(pingquantix)){
+#   if (pingquantix[i,1] > s2){
+#     pingquantix[i,1]=1;
+#   }
+#   else if (pingquantix[i,1] > s1 && pingquantix[i,1]< s2){
+#     pingquantix[i,1]=0.5;
+#   }
+#   else if (pingquantix[i,1] < -s2){
+#     pingquantix[i,1]=-1;
+#   }
+#   else if (pingquantix[i,1] > -s2 && pingquantix[i,1]< -s1){
+#     pingquantix[i,1]=-0.5;
+#   }
+#   else
+#     pingquantix[i,1]=0;
+# }
+#   
+quantization <- function (data,threshold){
+  if (nrow(data)%%2 == 1){
+    nrows = floor(nrow(data/2)+1);
   }
-  else if (pingquantix[i,1] > s1 && pingquantix[i,1]< s2){
-    pingquantix[i,1]=0.5;
+  else{
+    nrows = nrow(data)/2;
   }
-  else if (pingquantix[i,1] < -s2){
-    pingquantix[i,1]=-1;
-  }
-  else if (pingquantix[i,1] > -s2 && pingquantix[i,1]< -s1){
-    pingquantix[i,1]=-0.5;
-  }
-  else
-    pingquantix[i,1]=0;
+  mat = matrix(0,nrow = nrows,ncol=ncol(data));
+  mat[data>threshold]=1;
+  mat[data<-threshold]=-1;
+  mat[data<threshold && data>-threshold]=0;
+  # for(i in 1:nrows){
+  #   if (data[i,]>threshold){
+  #     mat[i,]=1;
+  #   }
+  #   else if (data[i,]<-threshold){
+  #     mat[i,]=-1;
+  #   }
+  #   else{
+  #     mat[i,]=0;
+  #   }
+  # }
+  return(mat);
 }
-  
+
+threshold = 150
+pingpongAcc[3018:3926,]
+quantiPingPongQuantGyro = quantization(pingpongGyr,threshold)
+quantiPingPongQuantGyro= quantiPingPongQuantGyro[3018:3926,]
+quantiPingPongQuantGyro
+plot(quantiPingPongQuantGyro)
+# plot(quantiPingPongAccGyro)
