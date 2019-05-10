728b9e64d2617193a9b7fe3f09dc8300228a3e42
tan0407 <45198471+tan0407@users.noreply.github.com>
2019-05-10 21:58:10 +0200

    Added the peakdetection windowed

--- Matlab/mainPeakDetection.m
+++ Matlab/mainPeakDetection.m
@@ -28,4 +28,78 @@ QuantiPingGyro = quantization(PingGyro,thresholdGyro);
 QuantiPingEMG = quantization(PingEMG,thresholdEMG);
 QuantiPingAcc = quantization(PingAcc,thresholdAcc);
 
+QuantiAspiGyro = quantization(AspiGyro,thresholdGyro);
+QuantiAspiEMG = quantization(AspiEMG,thresholdEMG);
+QuantiAspiAcc = quantization(AspiAcc,thresholdAcc);
+
+choix = 1;
+
 %PeakDetection Without Windows
+if choix == 0
+    [TSGyro,TSEMG,TSAcc] = peakDetection(QuantiEpiGyro,QuantiEpiEMG,QuantiEpiAcc);
+    [PeaksRunGyro,PeaksRunEMG,PeaksRunAcc] = peakDetection(QuantiRunGyro,QuantiRunEMG,QuantiRunAcc);
+    [PeaksBrushGyro,PeaksBrushEMG,PeaksBrushAcc] = peakDetection(QuantiBrushGyro,QuantiBrushEMG,QuantiBrushAcc);
+    [PeaksPingGyro,PeaksPingEMG,PeaksPingAcc] = peakDetection(QuantiPingGyro,QuantiPingEMG,QuantiPingAcc);
+    [PeaksBasketGyro,PeaksBasketEMG,PeaksBasketAcc] = peakDetection(QuantiBasketGyro,QuantiBasketEMG,QuantiBasketAcc);
+
+    if(PeaksRunGyro>0.9*TSGyro || PeaksRunEMG>0.9*TSEMG || PeaksRunAcc > 0.9*TSAcc)
+        isCrisis = true;
+    else isCrisis = false;
+    end
+
+%PeakDetection Windowed
+elseif choix ==1
+    
+    Window = 8000;
+    begin = floor(rand()*10000);
+    scale = size(QuantiEpiEMG,1)/size(QuantiEpiAcc,1);
+
+    CurrentWindowEpiAcc = QuantiEpiAcc(begin:begin+Window,:);
+    CurrentWindowEpiGyro = QuantiEpiGyro(begin:begin+Window,:);
+    CurrentWindowEpiEMG = QuantiEpiEMG(floor(begin*scale):floor((begin+Window)*scale),:);
+
+    begin = floor(rand()*10000);
+    scale = size(QuantiRunEMG,1)/size(QuantiRunAcc,1);
+
+    CurrentWindowRunAcc = QuantiRunAcc(begin:begin+Window,:);
+    CurrentWindowRunGyro = QuantiRunGyro(begin:begin+Window,:);
+    CurrentWindowRunEMG = QuantiRunEMG(floor(begin*scale):floor((begin+Window)*scale),:);
+    
+    begin = floor(rand()*10000);
+    scale = size(QuantiBrushEMG,1)/size(QuantiBrushAcc,1);
+
+    CurrentWindowBrushAcc = QuantiBrushAcc(begin:begin+Window,:);
+    CurrentWindowBrushGyro = QuantiBrushGyro(begin:begin+Window,:);
+    CurrentWindowBrushEMG = QuantiBrushEMG(floor(begin*scale):floor((begin+Window)*scale),:);
+    
+    begin = floor(rand()*10000);
+    scale = size(QuantiBasketEMG,1)/size(QuantiBasketAcc,1);
+
+    CurrentWindowBasketAcc = QuantiBasketAcc(begin:begin+Window,:);
+    CurrentWindowBasketGyro = QuantiBasketGyro(begin:begin+Window,:);
+    CurrentWindowBasketEMG = QuantiBasketEMG(floor(begin*scale):floor((begin+Window)*scale),:);
+    
+    begin = floor(rand()*10000);
+    scale = size(QuantiPingEMG,1)/size(QuantiPingAcc,1);
+
+    CurrentWindowPingAcc = QuantiBasketAcc(begin:begin+Window,:);
+    CurrentWindowPingGyro = QuantiBasketGyro(begin:begin+Window,:);
+    CurrentWindowPingEMG = QuantiBasketEMG(floor(begin*scale):floor((begin+Window)*scale),:);
+    
+
+    [TSGyro,TSEMG,TSAcc] = peakDetection(CurrentWindowEpiGyro,CurrentWindowEpiEMG,CurrentWindowEpiAcc);
+    [PeaksRunGyro,PeaksRunEMG,PeaksRunAcc] = peakDetection(CurrentWindowRunGyro,CurrentWindowRunEMG,CurrentWindowRunAcc);
+    [PeaksBrushGyro,PeaksBrushEMG,PeaksBrushAcc] = peakDetection(CurrentWindowBrushGyro,CurrentWindowBrushEMG,CurrentWindowBrushAcc);
+    [PeaksBasketGyro,PeaksBasketEMG,PeaksBasketAcc] = peakDetection(CurrentWindowBasketGyro,CurrentWindowBasketEMG,CurrentWindowBasketEMG);
+    [PeaksPingGyro,PeaksPingEMG,PeaksPingAcc] = peakDetection(CurrentWindowPingGyro,CurrentWindowPingEMG,CurrentWindowPingAcc);
+
+    if(PeaksPingGyro>0.9*TSGyro || PeaksPingEMG>0.9*TSEMG || PeaksPingAcc > 0.9*TSAcc)
+        isCrisis = true;
+    else isCrisis = false;
+    end
+
+end
+
+
+
+
